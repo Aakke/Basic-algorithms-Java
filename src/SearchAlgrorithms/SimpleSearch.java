f96f3d73bae3d63b2674a8a25c52b80fa8d112d8
Aleksi <7270773+Aakke@users.noreply.github.com>
2019-02-19 16:52:27 +0200

    LinearSearch
    Init commit for simple search algorithm for linear approach. Only skeleton of the class.

---
+++ src/SearchAlgrorithms/SimpleSearch.java
@@ -0,0 +1,26 @@
+package SearchAlgrorithms;
+
+/**
+ * Simple search algorithms for linear approach. 
+ * @author Aleksi Tani
+ *
+ */
+public class SimpleSearch {
+
+	
+	protected static void linearSearch() {
+		// TODO: Simple linear search
+	}
+	
+	
+	
+	/**
+	 * Main method used for testing algorithm.
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		// Initializing used array / string / list etc.
+		// Testing (System.out.println() etc.) for searching.
+	}
+
+}
